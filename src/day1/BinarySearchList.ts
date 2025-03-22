1898f5ee2da0da3075c51c3e55ac2604d14babf4
Luis David <luisdavidmirandav12@gmail.com>
2025-03-22 17:21:33 -0600

    what i learned in the course

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let hi = haystack.length;
+    do{
+        const m = Math.floor(low+(hi-low)/2);
+        let v = haystack[m];
+        if(v === needle){
+            return true;
+        }else if(v > needle){
+            hi = m;
+            
+        }else{
+            low = m +1;
+        }
+    }while(low < hi){
+        return false
+    }
 
 }
